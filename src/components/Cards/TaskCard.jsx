32d8cf327867160812de2539e422547c2c849c88
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-12-27 17:35:57 +0600

    Error solve in the task manager field

---
+++ src/components/Cards/TaskCard.jsx
@@ -0,0 +1,18 @@
+const TaskCard = ({
+  title,
+  description,
+  priority,
+  status,
+  progress,
+  createdAt,
+  dueDate,
+  assignedTo,
+  attachmentCount,
+  completedTodoCount,
+  todoChecklist,
+  onClick,
+}) => {
+  return <div>TaskCard</div>;
+};
+
+export default TaskCard;
--- src/pages/Admin/ManageTasks.jsx
+++ src/pages/Admin/ManageTasks.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import { LuFileSpreadsheet } from "react-icons/lu";
+import TaskStatusTab from "../../components/TaskStatusTab";
+import TaskCard from "../../components/Cards/TaskCard";
 
 const ManageTasks = () => {
   const [allTasks, setAllTasks] = useState([]);
@@ -19,19 +21,31 @@ const ManageTasks = () => {
           status: filterStatus === "All" ? "" : filterStatus,
         },
       });
+
       setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
 
-      // Map status Summary data with fixed labels and order
+      // Map status Summary data with exact keys from your API
       const statusSummary = response.data?.statusSummary || {};
+
       const statusArray = [
         {
           label: "All",
           count: statusSummary.all || 0,
         },
-        { label: "Pending", count: statusSummary.pending || 0 },
-        { label: "In Progress", count: statusSummary.inProgress || 0 },
-        { label: "Completed", count: statusSummary.completed || 0 },
+        {
+          label: "Pending",
+          count: statusSummary.pendingTasks || 0,
+        },
+        {
+          label: "In Progress",
+          count: statusSummary.inProgressTasks || 0,
+        },
+        {
+          label: "Completed",
+          count: statusSummary.completedTasks || 0,
+        },
       ];
+
       setTabs(statusArray);
     } catch (error) {
       console.error("Error fetching users:", error);
@@ -64,6 +78,42 @@ const ManageTasks = () => {
               Download Report
             </button>
           </div>
+          {tabs?.[0]?.count > 0 && (
+            <div className="flex items-center gap-3">
+              <TaskStatusTab
+                tabs={tabs}
+                activeTab={filterStatus}
+                setActiveTab={setFilterStatus}
+              />
+              <button
+                className="hidden lg:flex download-btn"
+                onClick={handleDownloadReport}
+              >
+                <LuFileSpreadsheet className="text-lg" />
+                Download Report
+              </button>
+            </div>
+          )}
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+          {allTasks?.map((item, index) => (
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              priority={item.priority}
+              status={item.status}
+              progress={item.progress}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
+              attachmentCount={item.attachments?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => handleClick(item)}
+            />
+          ))}
         </div>
       </div>
     </DashboardLayout>
